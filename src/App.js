9abc12ba2788500ee4cf37d3bcb0747c3ecba32d
Md Nazmul Alom <nazmulalom635@gmail.com>
2024-02-01 20:11:03 +0200

    employee name loaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [employees, setEmployees] = useState([])
+
+  const loadEmployees = async () => {
+    //load JSON data
+    const response = await axios.get('http://localhost:3002/employees')
+    console.log(response)
+    setEmployees(response.data) // [{}.{},.....]
+   }
+
+  useEffect( () => {
+    loadEmployees()
+  }, [])// runn only one time
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload Hello.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      employees.map(employee =>{
+        return  <p>{employee.firstName}</p>
+
+      })
+    }
     </div>
   );
 }
