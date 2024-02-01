5a29bc9fd4242da691616f42c6cdc8cd4bebe6eb
Md Nazmul Alom <nazmulalom635@gmail.com>
2024-02-01 19:21:04 +0200

    axios library added

---
+++ Employee.jsx
@@ -0,0 +1,17 @@
+import {MdEmail, MdPhone} from 'react-icons/md'
+import '../App.css'
+
+export default function Employee({employee}) {
+  return (
+    <div className="Employee">
+      <img src={employee.image} alt={employee.firstName} />
+      <div>
+        <p><b>{employee.lastName} {employee.firstName}</b></p>
+        <p>{employee.title}</p>
+        <p>{employee.department}</p>
+        <p><MdEmail/> {employee.email}</p>
+        <p><MdPhone/> {employee.phone}</p>
+      </div>
+    </div>
+  )
+}
